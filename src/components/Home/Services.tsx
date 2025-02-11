ad1ca551434740dc6b67abaa48d5f0303e768c80
sunam007 <asaduzzaman.sunam@gmail.com>
2025-02-11 12:31:17 +0600

    feat: refactor Portfolio and Services components for improved layout and styling

--- src/components/Home/Services.tsx
+++ src/components/Home/Services.tsx
@@ -7,38 +7,54 @@ const Services = () => {
   return (
     <Container>
       {/* Title */}
+
       <div className="grid grid-cols-1 mb-7">
         <SectionSubTitle>What we do</SectionSubTitle>
         <SectionTitle>Services</SectionTitle>
       </div>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+
+      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-3">
         <Link to="/services">
-          <div className="w-64 my-4">
+          <div className="w-60 md:w-64 xl:w-80 my-4">
             <div className="text-right relative z-50">
               <h4 className="service-title">Wedding Photography</h4>
             </div>
             <div className="relative service-img-div">
-              <img src="/src/assets/images/about.jpg" alt="" className="service-img"/>
+              <img
+                src="/src/assets/images/about.jpg"
+                alt=""
+                className="service-img"
+              />
             </div>
           </div>
         </Link>
+
         <Link to="/services">
-          <div className="w-64 my-4">
+          <div className="w-60 md:w-64 xl:w-80 my-4">
             <div className="text-right relative z-50">
               <h4 className="service-title">Wedding Photography</h4>
             </div>
             <div className="relative service-img-div">
-              <img src="/src/assets/images/about.jpg" alt="" className="service-img"/>
+              <img
+                src="/src/assets/images/about.jpg"
+                alt=""
+                className="service-img"
+              />
             </div>
           </div>
         </Link>
+
         <Link to="/services">
-          <div className="w-64 my-4">
+          <div className="w-60 md:w-64 xl:w-80 my-4">
             <div className="text-right relative z-50">
               <h4 className="service-title">Wedding Photography</h4>
             </div>
             <div className="relative service-img-div">
-              <img src="/src/assets/images/about.jpg" alt="" className="service-img"/>
+              <img
+                src="/src/assets/images/about.jpg"
+                alt=""
+                className="service-img"
+              />
             </div>
           </div>
         </Link>
